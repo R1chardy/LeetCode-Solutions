73c9a17300b3290b42598afbf602e19e5fcf24be
Richardy <113278314+R1chardy@users.noreply.github.com>
2022-10-12 18:23:31 -0500

    Time: 27 ms (84.59%), Space: 9.1 MB (81.35%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> ret = {};
     vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ret = {};
         vector<int> ans = {};
-        dfs(n, k, ans);
+        dfs(n, k, ans, ret);
         return ret;
     }
     
-    void dfs(int n, int k, vector<int> ans, int len = 0, int num = 1){
+    void dfs(int n, int k, vector<int>& ans, vector<vector<int>>& ret, int len = 0, int num = 1){
         if(len >= k){
             ret.push_back(ans);
             return;
@@ -15,7 +15,7 @@ public:
         else{
             for(int i = num; i <= n; i++){
                 ans.push_back(i);
-                dfs(n,k,ans,len+1,i+1);
+                dfs(n,k,ans,ret,len+1,i+1);
                 ans.pop_back();
             }
         }
