52c89fb3f1cecb8bbc071ab6046b2aadd7e2d295
Richardy <113278314+R1chardy@users.noreply.github.com>
2022-11-02 01:03:19 -0500

    Time: 94 ms (91.82%), Space: 21 MB (18.68%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        vector<vector<pair<int,int>>> dp(matrix.size(), vector<pair<int,int>>(matrix[0].size(), {0,0}));
-        if(matrix[0][0] == '1'){
-            dp[0][0] = {1,1};
+        int maxL = 0;
+        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), 0));
+        for(int i = 0; i < matrix.size(); i++){
+            dp[i][matrix[0].size()-1] = matrix[i][matrix[0].size()-1] - '0';
+            maxL = max(maxL, dp[i][matrix[0].size()-1]);
         }
-        for(int i = 1; i < matrix.size(); i++){
-            if(matrix[i][0] == '1'){
-                dp[i][0] = {1+dp[i-1][0].first, 1};
-            }
+        for(int j = 0; j < matrix[0].size(); j++){
+            dp[matrix.size()-1][j] = matrix[matrix.size()-1][j] - '0';
+            maxL = max(maxL, dp[matrix.size()-1][j]);
         }
-        for(int j = 1; j < matrix[0].size(); j++){
-            if(matrix[0][j] == '1'){
-                dp[0][j] = {1, 1+dp[0][j-1].second};
-            }
-        }
-        for(int i = 1; i < matrix.size(); i++){
-            for(int j = 1; j < matrix[0].size(); j++){
+        for(int i = matrix.size()-2; i >= 0; i--){
+            for(int j = matrix[0].size()-2; j >= 0; j--){
                 if(matrix[i][j] == '1'){
-                    dp[i][j] = {1+dp[i-1][j].first, 1+dp[i][j-1].second};
+                    dp[i][j] = 1 + min(dp[i+1][j], min(dp[i+1][j+1], dp[i][j+1]));
+                    maxL = max(maxL, dp[i][j]);
                 }
             }
         }
-        int num = 0;
-        for(int i = matrix.size()-1; i >= 0; i--){
-            for(int j = matrix[0].size()-1; j >= 0; j--){
-                for(int k = 0; k < min(dp[i][j].first, dp[i][j].second); k++){
-                    if(dp[i-k][j].second >= k+1 && dp[i][j-k].first >= k+1){
-                        num = max(num, k+1);
-                    }
-                    else{
-                        break;
-                    }
-                }
-            }
-        }
-        return num*num;
+        return maxL*maxL;
     }
 };
