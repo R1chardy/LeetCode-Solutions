ccc434c30dd9513e55710eaf7cc26b7c2ef9fdeb
Richardy <113278314+R1chardy@users.noreply.github.com>
2022-11-04 17:33:52 -0500

    Time: 125 ms (98.67%), Space: 93.4 MB (10.01%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<int> diffs;
-        for(int i = 1; i < prices.size(); i++){
-            diffs.push_back(prices[i] - prices[i-1]);
+        int minV = INT_MAX, maxV = 0;
+        for(auto x : prices){
+            minV = min(minV, x);
+            maxV = max(maxV, x - minV);
         }
-        int maxS = 0, currS = 0;
-        for(int i = 0; i < diffs.size(); i++){
-            currS += diffs[i];
-            maxS = max(maxS, currS);
-            if(currS < 0){
-                currS = 0;
-            }
-        }
-        return maxS;
+        return maxV;
     }
 };
