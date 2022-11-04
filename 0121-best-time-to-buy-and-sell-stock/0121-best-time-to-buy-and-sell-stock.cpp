e0a4b9590b33efe986d6f8f81a1022b9e60e6874
Richardy <113278314+R1chardy@users.noreply.github.com>
2022-11-04 17:31:33 -0500

    Time: 428 ms (5.01%), Space: 103.4 MB (5.08%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if(prices.size() <= 1){
-            return 0;
+        vector<int> diffs;
+        for(int i = 1; i < prices.size(); i++){
+            diffs.push_back(prices[i] - prices[i-1]);
         }
-        vector<int> diff;
-        for(int i = 0; i < prices.size() - 1; i++){
-            diff.push_back(prices[i+1] - prices[i]);
-        }
-        int ret = maxSubArray(diff);
-        return ret > 0? ret: 0;
-    }
-    
-    int maxSubArray(vector<int>& nums) {
-        int currSum = 0, maxSum = nums[0];
-        for(int i = 0; i < nums.size(); i++){
-            currSum += nums[i];
-            maxSum = max(currSum, maxSum);
-            if (currSum < 0){
-                currSum = 0;
+        int maxS = 0, currS = 0;
+        for(int i = 0; i < diffs.size(); i++){
+            currS += diffs[i];
+            maxS = max(maxS, currS);
+            if(currS < 0){
+                currS = 0;
             }
         }
-        return maxSum;
+        return maxS;
     }
 };
