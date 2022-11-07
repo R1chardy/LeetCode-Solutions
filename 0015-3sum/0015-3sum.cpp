3bd084f0d3b38dd25534d24f3511a3f2b7f2f1ad
Richardy <113278314+R1chardy@users.noreply.github.com>
2022-11-06 21:45:32 -0600

    Time: 577 ms (17.61%), Space: 19.9 MB (62.52%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        unordered_map<int, int> iMap;
-        for(int i = 0; i < nums.size(); i++){
-            iMap[nums[i]] = i;
-        }
-        vector<vector<int>> res;
-        for(int i = 0; i < nums.size() - 1; i++){
-            for(int j = i + 1; j < nums.size(); j++){
-                if(iMap[-(nums[i] + nums[j])] && iMap[-(nums[i] + nums[j])] > j){
-                    res.push_back({nums[i], nums[j], -(nums[i] + nums[j])});
-                }
-                j = iMap[nums[j]];
+        vector<vector<int>> ans;
+        for(int i = 0; i < nums.size()-2; i++){
+            if(i != 0 && nums[i] == nums[i-1]){
+                continue;
             }
-            i = iMap[nums[i]];
+            for(int j = i+1; j < nums.size()-1; j++){
+                if(j != i+1 && nums[j] == nums[j-1]){
+                    continue;
+                }
+                if(binary_search(nums.begin()+j+1, nums.end(), -1*(nums[i] + nums[j]))){
+                    ans.push_back({nums[i], nums[j], -1*(nums[i] + nums[j])});
+                }
+            }
         }
-        return res;
+        return ans;
     }
 };
