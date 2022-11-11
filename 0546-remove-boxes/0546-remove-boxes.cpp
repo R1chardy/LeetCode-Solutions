ece4bf54df3eb75161caf5144fcf80a05feaaa31
Richardy <113278314+R1chardy@users.noreply.github.com>
2022-11-11 03:24:23 -0600

    Time: 381 ms (61.66%), Space: 12.2 MB (61.40%) - LeetHub

---
+++ 0546-remove-boxes/0546-remove-boxes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[101][101][101];
+    int removeBoxes(vector<int>& boxes) {
+        memset(dp, 0, sizeof(dp));
+        return solve(boxes, 0, boxes.size()-1, 0);
+    }
+    
+    int solve(vector<int>& boxes, int l, int r, int k){
+        if(l > r){
+            return 0;
+        }
+        else if(dp[l][r][k]){
+            return dp[l][r][k];
+        }
+        int ans = (k+1)*(k+1) + solve(boxes, l+1, r, 0);
+        for(int i = l+1; i <= r; i++){
+            if(boxes[i] == boxes[l]){
+                ans = max(ans, solve(boxes, l+1, i-1, 0) + solve(boxes, i, r, k+1));
+            }
+        }
+        return dp[l][r][k] = ans;
+    }
+};
