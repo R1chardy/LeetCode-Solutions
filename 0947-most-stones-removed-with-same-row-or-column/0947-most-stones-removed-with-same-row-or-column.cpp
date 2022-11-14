6cdcb9b11684707393449ef4754c7dc1bc39e083
Richardy <113278314+R1chardy@users.noreply.github.com>
2022-11-14 14:32:17 -0600

    Time: 387 ms (41.79%), Space: 14.3 MB (73.89%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,70 @@
+class UnionFind {
+private:
+    int num;
+    int numComp;
+    int* sz;
+    int* root;
+    
+public:
+    UnionFind(int n) {
+        assert(n > 0);
+        num = numComp = n;
+        sz = new int[n];
+        root = new int[n];
+        for (int i = 0; i < n; i++) {
+            sz[i] = 1;
+            root[i] = i;
+        }
+    }
+    
+    int find(int x) {
+        assert(x >= 0 && x < num);
+        int head = x;
+        while (head != root[head]) {
+            head = root[head];
+        }
+        while (x != head) {
+            root[x] = head;
+            x = root[x];
+        }
+        return head;
+    }
+    
+    bool unify(int p, int q){
+        assert(p >= 0 && p < num && q >= 0 && q < num);
+        int root1 = find(p);
+        int root2 = find(q);
+        if(root1 == root2){
+            return false;
+        }
+        if(sz[root1] < sz[root2]){
+            root[root1] = root2;
+            sz[root2] += sz[root1];
+        }
+        else{
+            root[root2] = root1;
+            sz[root1] += sz[root2];
+        }
+        numComp--;
+        return true;
+    }
+    int components() const{
+        return numComp;
+    }
+};
+
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        UnionFind ds(stones.size());
+        for(int i = 0; i < stones.size(); i++){
+            for(int j = i+1; j < stones.size(); j++){
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
+                    ds.unify(i, j);
+                }
+            }
+        }
+        return stones.size()-ds.components();
+    }
+};
