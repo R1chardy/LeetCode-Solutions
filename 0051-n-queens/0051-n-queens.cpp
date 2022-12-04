48e15c615d0667bb9b3dd2f024a37dad6e1b65fa
Richardy <113278314+R1chardy@users.noreply.github.com>
2022-12-03 23:18:48 -0600

    Time: 11 ms (61.73%), Space: 7 MB (94.52%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,61 +1,70 @@
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ret;
-        vector<string> board(n, "");
-        string row = "";
-        for(int j = 0; j < n; j++){
-            row += ".";
-        }
-        for(int i = 0; i < n; i++){
-            board[i] = row;
-        }
-        for(int i = 0; i< n; i++){
-            board[0][i] = 'Q';
-            findSolution(board, 0, ret);
-            board[0][i] = '.';
-        }
-        return ret;
+    vector<vector<string>> solveNQueens(int n)
+{
+    vector<vector<string>> solutions;
+    vector<string> board(n, string(n, '.'));
+    vector<int> queens;
+
+    // Recursively place queens on the board
+    backtrack(solutions, board, queens, n);
+
+    return solutions;
+}
+
+void backtrack(vector<vector<string>>& solutions, vector<string>& board, vector<int>& queens, int n)
+{
+    // If we have placed all queens, add the current board configuration to the list of solutions
+    if (queens.size() == n)
+    {
+        solutions.push_back(board);
+        return;
     }
-    
-    void findSolution(vector<string>& board, int i, vector<vector<string>>& ret, int need = -1){
-        need = need == -1? board.size() - 1 : need;
-        if(need == 0){
-            ret.push_back(board);
-            return;
-        }
-        for(int x = i+1; x < board.size(); x++){
-            for(int y = 0; y < board.size(); y++){
-                if(isValid(board, x, y)){
-                    board[x][y] = 'Q';
-                    findSolution(board, x, ret, need - 1);
-                    board[x][y] = '.';
-                }
-            }
+
+    // Try placing a queen on each empty space
+    for (int i = 0; i < n; i++)
+    {
+        // Check if the current position is a valid location for a queen
+        if (isValid(board, queens, i))
+        {
+            // Place the queen on the board
+            board[queens.size()][i] = 'Q';
+            queens.push_back(i);
+
+            // Recursively place queens on the remaining empty spaces
+            backtrack(solutions, board, queens, n);
+
+            // Backtrack and remove the queen from the board
+            queens.pop_back();
+            board[queens.size()][i] = '.';
         }
     }
-    
-    bool isValid(vector<string>& board, int i, int j){
-        for(int x = 0; x < board.size(); x++){
-            if(i+x < board.size() && j+x < board.size() && board[i+x][j+x] == 'Q'){
-                return false;
-            }
-            if(i+x < board.size() && j-x >= 0 && board[i+x][j-x] == 'Q'){
-                return false;
-            }
-            if(i-x >= 0 && j+x < board.size() && board[i-x][j+x] == 'Q'){
-                return false;
-            }
-            if(i-x >= 0 && j-x >= 0 && board[i-x][j-x] == 'Q'){
-                return false;
-            }
-            if(x != j && board[i][x] == 'Q'){
-                return false;
-            }
-            if(x != i && board[x][j] == 'Q'){
-                return false;
-            }
+}
+
+bool isValid(vector<string>& board, vector<int>& queens, int col)
+{
+    int row = queens.size();
+
+    // Check if the queen is in the same column as any of the previously placed queens
+    for (int i = 0; i < row; i++)
+    {
+        if (queens[i] == col)
+        {
+            return false;
         }
-        return true;
     }
+
+    // Check if the queen is in the same diagonal as any of the previously placed queens
+    for (int i = 0; i < row; i++)
+    {
+        int diff = abs(queens[i] - col);
+        if (diff == row - i)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 };
