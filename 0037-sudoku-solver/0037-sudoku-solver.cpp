abe7cb08143c50b07bdaa949c57072880f63babe
Richardy <113278314+R1chardy@users.noreply.github.com>
2022-12-03 23:22:15 -0600

    Time: 70 ms (23.10%), Space: 6.4 MB (52.95%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,55 +1,84 @@
 class Solution {
 public:
-    bool solveSudoku(vector<vector<char>>& board){
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].size(); j++){
-                if(board[i][j] == '.'){
-                    //finding possible
-                    bool poss[10];
-                    memset(poss, true, 10);
-                    for(int x = 0; x < board[0].size(); x++){
-                        if(board[i][x] != '.'){
-                            poss[board[i][x] - '0'] = false;
+    void solveSudoku(vector<vector<char>>& board)
+{
+    backtrack(board);
+}
+
+bool backtrack(vector<vector<char>>& board)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            // If the current cell is empty, try filling it with a digit
+            if (board[i][j] == '.')
+            {
+                // Try filling the cell with each of the digits 1-9
+                for (int d = 1; d <= 9; d++)
+                {
+                    // Check if the current digit is valid for the current cell
+                    if (isValid(board, i, j, d))
+                    {
+                        // Fill the cell with the current digit
+                        board[i][j] = '0' + d;
+
+                        // Recursively fill the remaining empty cells
+                        if (backtrack(board))
+                        {
+                            return true;
                         }
+
+                        // If the current digit does not lead to a solution, backtrack and try a different digit
+                        board[i][j] = '.';
                     }
-                    for(int x = 0; x < board.size(); x++){
-                        if(board[x][j] != '.'){
-                            poss[board[x][j] - '0'] = false;
-                        }
-                    }
-                    for(int x = (i/3)*3; x < (i/3+1)*3; x++){
-                        for(int y = (j/3)*3; y < (j/3+1)*3; y++){
-                            if(board[x][y] != '.'){
-                                poss[board[x][y] - '0'] = false;
-                            }
-                        }
-                    }
-                    //false if nothing possible
-                    int numT = 0;
-                    for(int x = 1; x < 10; x++){
-                        if(poss[x]){
-                            numT++;
-                        }
-                    }
-                    if(numT == 0){
-                        return false;
-                    }
-                    //backtrack
-                    for(int x = 1; x < 10; x++){
-                        if(poss[x]){
-                            board[i][j] = '0'+x;
-                            if(solveSudoku(board)){
-                                return true;
-                            }
-                            board[i][j] = '.';
-                        }
-                    }
-                    //tried all possible but was not successful
-                    return false;
                 }
+
+                // If no digit is valid for the current cell, return false to backtrack
+                return false;
             }
         }
-        //reached end of board successfully
-        return true;
     }
+
+    // If all cells are filled, return true to indicate that a solution is found
+    return true;
+}
+
+bool isValid(vector<vector<char>>& board, int row, int col, int d)
+{
+    // Check if the current digit is already present in the current row
+    for (int i = 0; i < 9; i++)
+    {
+        if (board[row][i] - '0' == d)
+        {
+            return false;
+        }
+    }
+
+    // Check if the current digit is already present in the current column
+    for (int i = 0; i < 9; i++)
+    {
+        if (board[i][col] - '0' == d)
+        {
+            return false;
+        }
+    }
+
+    // Check if the current digit is already present in the current 3x3 sub-box
+    int rowStart = (row / 3) * 3;
+    int colStart = (col / 3) * 3;
+    for (int i = rowStart; i < rowStart + 3; i++)
+    {
+        for (int j = colStart; j < colStart + 3; j++)
+        {
+            if (board[i][j] - '0' == d)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 };
