7f72304e273ae2ffba96f2587503f55c41bbfddc
Richardy <113278314+R1chardy@users.noreply.github.com>
2023-03-10 04:06:47 -0600

    Time: 2458 ms (10.50%), Space: 435.6 MB (10.41%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -5,8 +5,6 @@ public:
             [](auto x, auto y){
                 return x[0] == y[0]? x[1] < y[1] : x[0] >= y[0];
             });
-        int w = envelopes[0][0];
-        int h = envelopes[0][1];
         vector<int> dp(envelopes.size(), 0);
         int len = 0;
         for(int i = 0; i < envelopes.size(); i++){
