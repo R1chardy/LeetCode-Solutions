3852622f0deb0798d8d89e7f2a2b83f7b24b7ddb
Richardy <113278314+R1chardy@users.noreply.github.com>
2023-03-10 04:09:14 -0600

    Time: 2804 ms (9.23%), Space: 484.8 MB (9.59%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -3,12 +3,12 @@ public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(), envelopes.end(),
             [](auto x, auto y){
-                return x[0] == y[0]? x[1] < y[1] : x[0] >= y[0];
+                return x[0] == y[0]? x[1] > y[1] : x[0] < y[0];
             });
         vector<int> dp(envelopes.size(), 0);
         int len = 0;
         for(int i = 0; i < envelopes.size(); i++){
-            int les = lower_bound(dp.begin(), dp.begin()+len, envelopes[i][1], greater<int>())-dp.begin();
+            int les = lower_bound(dp.begin(), dp.begin()+len, envelopes[i][1])-dp.begin();
             if(les == len){
                 len++;
             }
