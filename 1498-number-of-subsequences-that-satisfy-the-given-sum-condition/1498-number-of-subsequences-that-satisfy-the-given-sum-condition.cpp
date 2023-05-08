69f8a30ced23961892e6b8cf0e0b149c056de719
Richardy <113278314+R1chardy@users.noreply.github.com>
2023-05-07 21:39:20 -0700

    Time: 174 ms (20.17%), Space: 49.8 MB (70.23%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -5,7 +5,7 @@ public:
         int l = 0, r = nums.size()-1, ans = 0, mod = 1e9+7;
         vector<int> pows(nums.size()+1,1);
         for(int i = 1; i <= nums.size(); i++){
-            pows[i] = (pows[i-1] * 2)%mod;
+            pows[i] = (pows[i-1]*2)%mod;
         }
         while(l <= r){
             if(nums[l] + nums[r] > target){
