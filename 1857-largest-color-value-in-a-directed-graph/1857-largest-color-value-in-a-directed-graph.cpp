6631ca5a0b7cbcf88860a2eb13395c41364cd18c
Richardy <113278314+R1chardy@users.noreply.github.com>
2023-05-19 23:43:57 -0700

    Time: 752 ms (18.90%), Space: 226.2 MB (6.35%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -5,49 +5,40 @@ public:
         for(auto& edge : edges){
             mp[edge[0]].push_back(edge[1]);
         }
-        
         vector<bool> visited(colors.length());
         vector<bool> curStack(colors.length());
         vector<vector<int>> dp(colors.length(), vector<int>(26));
-        
         for(int i = 0; i < colors.length(); i++){
             if(solve(colors, mp, visited, curStack, dp, i)){
                 return -1;
             }
         }
-        
         int ret = 0;
         for(auto& x : dp){
             for(auto& y : x){
                 ret = max(ret, y);
             }
         }
-        
         return ret;
     }
     
-    bool solve(const string& colors, unordered_map<int,vector<int>>& mp, vector<bool>& visited, vector<bool>& curStack, vector<vector<int>>& dp, int i){
+    bool solve(string& colors, unordered_map<int,vector<int>>& mp, vector<bool>& visited, vector<bool>& curStack, vector<vector<int>>& dp, int& i){
         if(curStack[i] && visited[i]){
             return true;
         }
-        
         if(visited[i]){
             return false;
         }
-        
         visited[i] = true;
         curStack[i] = true;
-        
         for(auto& x : mp[i]){
             if(solve(colors, mp, visited, curStack, dp, x)){
                 return true;
             }
-            
             for(int y = 0; y < 26; y++){
                 dp[i][y] = max(dp[i][y], dp[x][y]);
             }
         }
-        
         dp[i][colors[i]-'a']++;
         curStack[i] = false;
         return false;
