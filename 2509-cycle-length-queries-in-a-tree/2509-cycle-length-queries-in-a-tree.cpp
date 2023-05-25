31f7ec00d3ca3b0178fa3f06f1796a1205e356b5
Richardy <113278314+R1chardy@users.noreply.github.com>
2023-05-24 20:32:43 -0700

    Time: 254 ms (55.38%), Space: 66 MB (85.26%) - LeetHub

--- 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.cpp
+++ 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.cpp
@@ -3,29 +3,21 @@ public:
     vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) {
         vector<int> ans;
         for(auto& query : queries){
-            vector<int> path1 = solve(query[0]);
-            vector<int> path2 = solve(query[1]);
-            while(!path1.empty() && !path2.empty() && path1.back() == path2.back()){
-                path1.pop_back();
-                path2.pop_back();
+            int x = 0, y = 0;
+            int a = query[0];
+            int b = query[1];
+            while(a != b){
+                if(a > b){
+                    a = a/2;
+                    x++;
+                }
+                else{
+                    b = b/2;
+                    y++;
+                }
             }
-            ans.push_back(path1.size() + path2.size() + 1);
+            ans.push_back(1+x+y);
         }
         return ans;
-    }
-    
-    vector<int> solve(int n){
-        vector<int> ret;
-        while(n != 1){
-            ret.push_back(n);
-            if(n%2){
-                n = (n-1)/2;
-            }
-            else{
-                n = n/2;
-            }
-        }
-        ret.push_back(1);
-        return ret;
     }
 };
