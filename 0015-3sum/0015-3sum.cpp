20c97abbff772595d7395bc8fb6aadf359e13d20
Richardy <113278314+R1chardy@users.noreply.github.com>
2023-05-24 23:46:50 -0700

    Time: 551 ms (35.41%), Space: 25 MB (59.11%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -8,6 +8,9 @@ public:
             val2idx[nums[i]] = i;
         }
         for(int i = 0; i < nums.size()-2; i++){
+            if(nums[i] > 0){
+                break;
+            }
             for(int j = i+1; j < nums.size()-1; j++){
                 if(val2idx.count(-nums[i]-nums[j]) && val2idx[-nums[i]-nums[j]] > j){
                     ans.push_back({nums[i], nums[j], -nums[i]-nums[j]});
