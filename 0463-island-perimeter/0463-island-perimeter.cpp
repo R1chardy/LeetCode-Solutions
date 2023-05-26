3310ff120204056612de2ec83222db005324656a
Richardy <113278314+R1chardy@users.noreply.github.com>
2023-05-25 18:47:05 -0700

    Time: 114 ms (80.30%), Space: 96.3 MB (35.58%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if(grid[i][j] == 1){
+                    return dfs(grid, i, j);
+                }
+            }
+        }
+        return 0;
+    }
+    
+    int dfs(vector<vector<int>>& grid, int i, int j){
+        
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0){
+            return 1;
+        }
+        else if(grid[i][j] == 2){
+            return 0;
+        }
+        else{
+            grid[i][j] = 2;
+            return dfs(grid, i+1, j) + dfs(grid, i-1, j) + dfs(grid, i, j+1) + dfs(grid, i, j-1);
+        }
+    }
+};
