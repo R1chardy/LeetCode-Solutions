09e8aac8d7cdc2762503d88c79df95ebf2e85d25
Richardy <113278314+R1chardy@users.noreply.github.com>
2023-05-29 21:45:49 -0700

    Time: 4 ms (87.52%), Space: 6 MB (77.66%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        std::string roman_chars[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    int int_values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-    // Initialize the result string
-    std::string result;
-
-    // Loop through the integers and Roman numerals, adding the Roman numeral
-    // to the result string whenever the integer is less than or equal to the
-    // number we're trying to convert
-    for (int i = 0; i < sizeof(int_values) / sizeof(int); i++) {
-      while (num >= int_values[i]) {
-        result += roman_chars[i];
-        num -= int_values[i];
-      }
-    }
-
-    // Return the result
-    return result;
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
+        return ths[num/1000] + hrns[num%1000/100] + tens[num%100/10] + ones[num%10];
     }
 };
