b5eddaabb56493ae50ae89bcab07efeb37de3405
Richardy <113278314+R1chardy@users.noreply.github.com>
2023-09-19 03:24:01 -0500

    Time: 2 ms (88.30%), Space: 10.8 MB (5.32%) - LeetHub

---
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+        int mdist = abs(target[0]) + abs(target[1]);
+        for(auto& ghost : ghosts){
+            if(abs(ghost[0]-target[0]) + abs(ghost[1]-target[1]) <= mdist){
+                return false;
+            }
+        }
+        return true;
+    }
+};
