e0c35dbc567c34d46953913666773e5404f2f73e
Richardy <113278314+R1chardy@users.noreply.github.com>
2023-12-14 20:04:37 -0600

    Time: 10 ms (62.92%), Space: 11.3 MB (35.41%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_map<string,int> mp;
+        for(auto& path : paths){
+            mp[path[0]]++;
+        }
+        for(auto& path : paths){
+            if(!mp.contains(path[1])){
+                return path[1];
+            }
+        }
+        return "";
+    }
+};
