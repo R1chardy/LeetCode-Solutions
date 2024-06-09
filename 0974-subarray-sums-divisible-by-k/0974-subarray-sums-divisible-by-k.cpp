bebdb2c3e083187b2225826a94b74de3fa6a1380
Richardy <113278314+R1chardy@users.noreply.github.com>
2024-06-09 16:55:34 -0700

    Time: 44 ms (44.54%), Space: 37.7 MB (5.09%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        map<int,int> rems = {{0,1}};
-        
-        vector<int> prefix(nums.size()+1, 0);
-        prefix[0]=0;
-        int pre = 0;
+        vector<int> pre;
+        int s = 0;
         for(int i = 0; i < nums.size(); i++){
-            pre += nums[i];
-            prefix[i+1] = pre;
+            pre.push_back(s);
+            s += nums[i];
         }
+        pre.push_back(s);
         
-        for(auto x : prefix){
-            cout << x << " ";
-        }
-        
+        unordered_map<int,int> xd;
+        xd[0]=1;
         int count = 0;
-        for(int i = 1; i < prefix.size(); i++){
-            count += rems[prefix[i]%k < 0? prefix[i]%k + k : prefix[i]%k];
-            rems[prefix[i]%k < 0? prefix[i]%k + k : prefix[i]%k]++;
+        for(int i = 1; i < pre.size(); i++){
+            int idx = pre[i]%k < 0? pre[i]%k + k : pre[i]%k;
+            count += xd[idx];
+            xd[idx]++;
         }
         return count;
     }
